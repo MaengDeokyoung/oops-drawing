626873c77e68f8eff95337b14b999cc63a8ff2cc
Maeng <landkid12@gmail.com>
2020-05-05 21:43:52 +0900

    complete instagram music

--- src/js/wave/wave-drawing.js
+++ src/js/wave/wave-drawing.js
@@ -85,9 +85,10 @@ window.onload = () => {
             dataArray = new Uint8Array(bufferLength);
 
             const request = new XMLHttpRequest();
-            request.open('GET', './../img/instagram_cover.ogg');
+            request.open('GET', './../img/instagram_cover.mp3');
             request.responseType = 'arraybuffer';
             request.onload = (event) => {
+
                 audioSource = audioCtx.createBufferSource();
                 analyser = audioCtx.createAnalyser();
 
